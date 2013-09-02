db8b9f2a17b282f3f42ca7d4ae8ebae95c29e053
Björn Wilmsmann <bjoern@wilmsmann.de>
2013-09-03 00:36:50 +0200

    Testing and reverting stuff ...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,22 @@
 'use strict';
 
-angular.module('TimeSheetsApp', ['pascalprecht.translate'])
-  .config(function ($routeProvider, $translateProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-    $translateProvider
-      .translations('de_DE', {
-        EMPLOYEE: 'Mitarbeiter',
-        DATE: 'Datum',
-        HOURS: 'Stunden'
-      });
-    $translateProvider.preferredLanguage('de_DE');
-  });
+var module = angular.module(
+  'TimeSheetsApp',
+  ['pascalprecht.translate']
+).config(function ($routeProvider, $translateProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+  $translateProvider
+    .translations('de_DE', {
+      EMPLOYEE: 'Mitarbeiter',
+      DATE: 'Datum',
+      HOURS: 'Stunden'
+    });
+  $translateProvider.preferredLanguage('de_DE');
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
-angular.module('TimeSheetsApp')
-  .controller('MainCtrl', function ($scope) {
+angular.module('TimeSheetsApp').controller(
+	'MainCtrl',
+	function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      { key: 'HTML5 Boilerplate', value: 'HTML5 Boilerplate' },
+      { key: 'AngularJS', value: 'AngularJS' },
+      { key: 'Karma', value: 'Karma' }
     ];
-  });
+  }
+);
