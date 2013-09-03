6ed291ba39c9e128235f07c45c847c1d1f6bd6a7
Björn Wilmsmann <bjoern@wilmsmann.de>
2013-09-03 21:32:25 +0200

    Clean up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,27 @@
 'use strict';
 
 angular.module('TimeSheetsApp', ['pascalprecht.translate', 'ui.select2'])
-.config(function ($routeProvider, $translateProvider) {
-	$routeProvider
-	.when('/', {
-		templateUrl: 'views/main.html',
-		controller: 'MainCtrl'
-	})
-	.otherwise({
-		redirectTo: '/'
+	.config(function ($routeProvider, $translateProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+		
+		$translateProvider.translations('de_DE', {
+			EMPLOYEE: 'Mitarbeiter',
+			DATE: 'Datum',
+			HOURS: 'Stunden',
+			SUBMIT: 'Abschicken',
+			DOMAIN: 'Domain',
+			API_KEY: 'API Key',
+			PROJECT: 'Projekt',
+			ENTER_DOMAIN: 'Bitte geben Sie Ihre Domain ein.',
+			ENTER_API_KEY: 'Bitte geben Sie Ihren API Key ein.',
+			SELECT_PROJECT: 'Bitte wählen Sie ein Projekt aus.',
+		});
+		$translateProvider.preferredLanguage('de_DE');
 	});
-	
-	$translateProvider.translations('de_DE', {
-		EMPLOYEE: 'Mitarbeiter',
-		DATE: 'Datum',
-		HOURS: 'Stunden'
-	});
-	$translateProvider.preferredLanguage('de_DE');
-});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,16 @@
 
 angular.module('TimeSheetsApp').controller(
 	'MainCtrl',
-	function ($scope) {
+	function ($scope, $http) {
     $scope.awesomeThings = [
       { key: 'HTML5 Boilerplate', value: 'HTML5 Boilerplate' },
       { key: 'AngularJS', value: 'AngularJS' },
       { key: 'Karma', value: 'Karma' }
     ];
+
+    $http.defaults.useXDomain = true;
+    $http.get('https://' + $scope.domain + '.mite.yo.lk/projects.xml?api_key=' + $scope.apiKey)
+	    .success(function(data) {
+	    });
   }
 );
