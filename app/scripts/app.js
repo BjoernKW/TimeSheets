dd935a4cc099fd5133ce97c74a1565340cb2a158
Björn Wilmsmann <bjoern@wilmsmann.de>
2013-09-03 09:52:02 +0200

    Clean up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,20 @@
 'use strict';
 
-angular.module(
-  'TimeSheetsApp',
-  ['pascalprecht.translate']
-).config(function ($routeProvider, $translateProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-  $translateProvider
-    .translations('de_DE', {
-      EMPLOYEE: 'Mitarbeiter',
-      DATE: 'Datum',
-      HOURS: 'Stunden'
-    });
-  $translateProvider.preferredLanguage('de_DE');
+angular.module('TimeSheetsApp', ['pascalprecht.translate', 'ui.select2'])
+.config(function ($routeProvider, $translateProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'MainCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+	
+	$translateProvider.translations('de_DE', {
+		EMPLOYEE: 'Mitarbeiter',
+		DATE: 'Datum',
+		HOURS: 'Stunden'
+	});
+	$translateProvider.preferredLanguage('de_DE');
 });
